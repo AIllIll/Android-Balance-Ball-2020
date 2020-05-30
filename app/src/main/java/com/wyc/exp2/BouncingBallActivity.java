bb6368f53ac96c567c1a1985ec922de1e6849ac8
WyC <864147974@qq.com>
2020-05-30 21:15:51 +0800

    触碰屏幕边缘后改变小球颜色

--- app/src/main/java/com/wyc/exp2/BouncingBallActivity.java
+++ app/src/main/java/com/wyc/exp2/BouncingBallActivity.java
@@ -11,6 +11,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -36,6 +37,28 @@ public class BouncingBallActivity extends Activity implements SensorEventListene
 	private float mAx; // acceleration along x axis
 	private float mAy; // acceleration along y axis
 	private final float mDeltaT = 0.5f; // imaginary time interval between each acceleration updates
+
+	// 小球的颜色
+	private int mBallColor = 0xFF78838B; // 默认色
+	// 小球的数组
+	private final int[] colorList = {
+			0xFF78838B,
+			0xFFDCF5F5,
+			0xFFCDEBFF,
+			0xFFFF0000,
+			0xFF2A2AA5,
+			0xFF87B8DE,
+			0xFFA09E5F,
+			0xFF00FF7F,
+			0xFF7295EE,
+			0xFFE0FFFF,
+			0xFF621C8B,
+			0xFFFF82AB,
+			0xFF800000,
+			0xFFCBC0FF,
+			0xFF0000FF,
+			0xFFCDA66C
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -104,6 +127,7 @@ public class BouncingBallActivity extends Activity implements SensorEventListene
 		private final int RADIUS = 50;
 		private final float FACTOR_BOUNCEBACK = 0.75f;
 
+		// 小球中心，可以用来判断屏幕边缘碰撞
 		private int mXCenter;
 		private int mYCenter;
 		private RectF mRectF;
@@ -148,27 +172,38 @@ public class BouncingBallActivity extends Activity implements SensorEventListene
 			mXCenter += (int)(mDeltaT * (mVx + 0.5 * mAx * mDeltaT));
 			mYCenter += (int)(mDeltaT * (mVy + 0.5 * mAy * mDeltaT));
 
+			// 触碰左侧
 			if(mXCenter < RADIUS)
 			{
 				mXCenter = RADIUS;
 				mVx = -mVx * FACTOR_BOUNCEBACK;
+				updateBallColor();
 			}
-
+			// 触碰上方
 			if(mYCenter < RADIUS)
-			{  mYCenter = RADIUS;  mVy = -mVy * FACTOR_BOUNCEBACK;  }
+			{  mYCenter = RADIUS;  mVy = -mVy * FACTOR_BOUNCEBACK;
+				updateBallColor(); }
+			// 触碰右侧
 			if(mXCenter > mWidthScreen - RADIUS)
 			{
 				mXCenter = mWidthScreen - RADIUS;
 				mVx = -mVx * FACTOR_BOUNCEBACK;
+				updateBallColor();
 			}
-
+			// 触碰下方
 			if(mYCenter > mHeightScreen - 2 * RADIUS)
 			{
 				mYCenter = mHeightScreen - 2 * RADIUS;
 				mVy = -mVy * FACTOR_BOUNCEBACK;
+				updateBallColor();
 			}
 
 			return true;
+		}
+
+		// 判断是否触及屏幕边缘并修改小球颜色
+		public void updateBallColor(){
+			mBallColor += colorList[(int)(1+Math.random()*(15-1+1))];
 		}
 
 		// update the canvas
@@ -177,7 +212,8 @@ public class BouncingBallActivity extends Activity implements SensorEventListene
 			if(mRectF != null && destroyed == false )
 			{
 				mRectF.set(mXCenter - RADIUS, mYCenter - RADIUS, mXCenter + RADIUS, mYCenter + RADIUS);
-				canvas.drawColor(0XFF000000);
+				canvas.drawColor(0xFF000000); // 屏幕底色
+				mPaint.setColor(mBallColor); // 设置小球颜色
 				canvas.drawOval(mRectF, mPaint);
 			}
 		}
